d53fa568a416268ae51e16a4d14979e6e09c057f
Basil khowaja <122356913+basilk15@users.noreply.github.com>
2025-08-12 12:22:44 +0500

    Add files via upload

---
+++ src/wifiTime.h
@@ -0,0 +1,34 @@
+uint64_t  startEpochOffset = 0;
+
+
+void setupWiFi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWi-Fi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+}
+
+void setupTime(){
+  lcdPrintAt(0, 0, "Setting Time");
+  configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+
+  while (time(nullptr) < 24 * 3600) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  startEpochOffset = static_cast<int64_t>(time(nullptr)) * 1000LL - (int64_t)millis();
+  Serial.printf("\nEpoch offset (ms): %lld\n", startEpochOffset);
+  lcdPrintAt(0, 0, "Time Synced");
+}
+
+long getWifiStrength(){
+  return WiFi.RSSI();
+}
+
